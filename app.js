53b9e193aeaa4fa4b984ebf9b042779410464954
n0n0b0 <152247342+n0n0b0@users.noreply.github.com>
2023-11-28 13:21:21 +0800

    app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const loginApi = qs.loginApi
 // 签到并提交每日体温报告 
 login().then((token) => {
   sign(token)
-  studentReportInfo(token)
 })
 
 //登录
@@ -37,22 +36,6 @@ function sign(token) {
   })
 }
 
-//日报提交
-function studentReportInfo(token) {
-  const studentReportApi = qs.studentReportApi(token)
-  const studentReportCommitApi = qs.studentReportCommitApi(token)
-  axios.get(studentReportApi).then((res) => {
-    if (res.data.code === 20000) {
-      const { family_name, family_phone } = res.data.data.list[0]
-      const reportForm = qs.reportdata(family_name, family_phone)
-      axios.post(studentReportCommitApi, reportForm).then((res) => {
-        console.log(res.data.code + ',' + res.data.message)
-        wechatSend('习讯云日报提交', res.data.message)
-        // sendEmail('习讯云日报提交', res.data.message)
-      })
-    }
-  })
-}
 
 //推送微信通知
 function wechatSend(type, msg) {
